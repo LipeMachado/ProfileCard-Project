b8cc6238b55639d9085e97d080d139d6c885283f
LipeMachado <silvalipe765@gmail.com>
2023-05-17 01:03:47 -0300

    Init Commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { GlobalStyle } from "./Style/GlobalStyle";
+import { BoxProfile } from './Components/BoxProfile'
+
+export function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <BoxProfile />
+    </>
+  )
+}
---
+++ src/Style/GlobalStyle.tsx
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components';
+import bgBottomPattern from '../Assets/bgPatternBottom.png';
+
+export const GlobalStyle = createGlobalStyle`
+
+    *{
+        margin: 0;
+        padding: 0;
+    }
+
+    body {
+        background-color: hsl(185, 75%, 39%);
+        background-image: url(${bgBottomPattern});
+        background-repeat: no-repeat;
+        background-size: 1000px;
+        background-position: 140% -125%;
+    }
+`;
